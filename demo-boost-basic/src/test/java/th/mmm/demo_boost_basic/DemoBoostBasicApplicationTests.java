844db14bf6a81f62516a5f8c62b8066aa879cb19
Ye Myat Min <yemyatmin192@gmail.com>
2025-10-19 22:57:10 +0700

    Add files via upload

---
+++ demo-boost-basic/src/test/java/th/mmm/demo_boost_basic/DemoBoostBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package th.mmm.demo_boost_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBoostBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
