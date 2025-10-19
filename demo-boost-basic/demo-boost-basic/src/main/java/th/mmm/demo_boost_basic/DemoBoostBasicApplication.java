258fe09f4ace5e13daad63fefdebea6350e5d57e
Ye Myat Min <yemyatmin192@gmail.com>
2025-10-19 21:50:37 +0700

    finish

---
+++ demo-boost-basic/demo-boost-basic/src/main/java/th/mmm/demo_boost_basic/DemoBoostBasicApplication.java
@@ -0,0 +1,13 @@
+package th.mmm.demo_boost_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBoostBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBoostBasicApplication.class, args);
+	}
+
+}
